66fa54d59fc370d14b15f7ed3001e850432968b6
ganjianping <ganjpxm@gmail.com>
2013-08-28 22:02:01 +0800

    1

---
+++ jpi/test.h
@@ -0,0 +1,17 @@
+//
+//  JpViewController.h
+//  jpi
+//
+//  Created by lenben on 6/7/13.
+//  Copyright (c) 2013 ganjp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginSbViewController : UIViewController <UITextFieldDelegate>
+
+@property (copy, nonatomic) NSString *userName;
+@property (copy, nonatomic) NSString *password;
+
+
+@end
